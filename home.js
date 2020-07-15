82c80d8ee42617cdeaffc6863aeb887b2b7829fe
lokeshdissrani <lokeshdissrani@gmail.com>
2020-07-15 20:10:43 +0530

    UI improvising pending

--- home.js
+++ home.js
@@ -1,7 +1,8 @@
 var tempdate=new Date();
 var ourdate=tempdate.getDate();
-var ourmonth=tempdate.getMonth();
+var ourmonth=tempdate.getMonth()+1;
 var ouryear=tempdate.getFullYear();
+console.log(tempdate)
 var date=document.getElementById("date");
 console.log(date.value);
 if(ourmonth<10){
@@ -21,16 +22,56 @@ var incomesubmit=document.getElementById("incomesubmit");
 incomesubmit.addEventListener("click",addincome);
 var budgetsubmit=document.getElementById("budgetsubmit");
 budgetsubmit.addEventListener("click",addbudget);
-var datesubmit=document.getElementById("date");
-datesubmit.addEventListener("click",datewise);
+var datesubmit=document.getElementById("datesubmit");
+datesubmit.addEventListener("click",datenew);
 var logout=document.getElementById("logout");
 logout.addEventListener("click",exit);
 function exit(){
     window.location.href="index.html";
 }
+function datenew(e){
+    e.preventDefault();
+    datewise();
+}
 function datewise(){
-    var date=document.getElementById("date").value;
-    var datearray=date.split("-");
+    var i1=document.createElement("i")
+    var i2=document.createElement("i")
+    var i3=document.createElement("i")
+    var i4=document.createElement("i")
+    var i5=document.createElement("i")
+    var i6=document.createElement("i")
+    var i7=document.createElement("i")
+    i1.setAttribute("class","fas fa-money-bill-wave");
+    i2.setAttribute("class","fas fa-money-check");
+    i3.setAttribute("class","fas fa-wallet");
+    i4.setAttribute("class","fas fa-dollar-sign");
+    i5.setAttribute("class","fas fa-rupee-sign");
+    i6.setAttribute("class","fas fa-hand-holding-usd");
+    i7.setAttribute("class","fas fa-file-invoice-dollar");
+    i1.style="font-size:60px;color:red;"
+    i2.style="font-size:60px;color:green;"
+    i3.style="font-size:60px;color:indigo;"
+    i4.style="font-size:60px;color:cyan;"
+    i5.style="font-size:60px;color:skyblue;"
+    i6.style="font-size:60px;color:orange;"
+    i7.style="font-size:60px;color:grey;"
+    var idiv1=document.createElement("div");
+    var idiv2=document.createElement("div");
+    var idiv3=document.createElement("div");
+    var idiv4=document.createElement("div");
+    var idiv5=document.createElement("div");
+    var idiv6=document.createElement("div");
+    var idiv7=document.createElement("div");
+    idiv1.append(i1);
+    idiv2.append(i2);
+    idiv3.append(i3);
+    idiv4.append(i4);
+    idiv5.append(i5);
+    idiv6.append(i6);
+    idiv7.append(i7);
+
+    var date1=document.getElementById("date").value;
+    var datearray=date1.split("-");
     var year=datearray[0];
     var month=datearray[1];
     if(!(year in currentuser)){
@@ -76,33 +117,51 @@ function datewise(){
         var divlimit=document.createElement("div");
         if(income>=expense){
             divsavings.style.color="green";
-            divsavings.textContent="You are saving"+(income-expense);
+            divsavings.textContent="SAVINGS "+(income-expense);
         }
         else{
             divsavings.style.color="red";
-            divsavings.textContent="You are not saving and have loan of"+(expense-income);
+            divsavings.textContent="DEBT "+(expense-income);
         }
         if(budget>=expense){
             divlimit.style.color="green";
-            divlimit.textContent="You are spending within limit! So expect good savings so plus amount is"+(budget-expense);
+            divlimit.textContent="INLIMIT "+(budget-expense);
         }
         else{
             divlimit.style.color="red";
-            divlimit.textContent="You are not spending within limit! So dont expect savings so overlimit amount is"+(budget-expense);
+            divlimit.textContent="OVERLIMIT "+(budget-expense);
         }
-        divincome.textContent="Total income this month"+income;
-        divbudget.textContent="Budget You set for this month is"+budget;
-        divexpense.textContent="So expenses till now is"+expense;
+        divincome.textContent="INCOME "+income;
+        divbudget.textContent="BUDGET "+budget;
+        divexpense.textContent="EXPENSES "+expense;
         var monthdiv=document.createElement("div");
-        monthdiv.textContent="Details for month:"+month+"& year:"+year;
+        monthdiv.textContent="MONTH: "+month+" YEAR: "+year;
         var maindiv=document.getElementById("maindiv");
         maindiv.textContent="";
-        maindiv.append(divname,monthdiv,divincome,divexpense,divbudget,divsavings,divlimit,summarydiv);
+        divname.style.float="left";
+        idiv7.style.float="left";
+        monthdiv.style.float="left";
+        idiv1.style.float="left";
+        divincome.style.float="left";
+        idiv2.style.float="left";
+        divexpense.style.float="left";
+        idiv3.style.float="left";
+        divbudget.style.float="left";
+        idiv4.style.float="left";
+        divsavings.style.float="left";
+        idiv5.style.float="left";
+        divlimit.style.float="left";
+        idiv6.style.float="left";
+        summarydiv.style.marginTop="50px";
+        var tempdiv=document.createElement("div");
+        tempdiv.textContent="DETAILS"
+        maindiv.append(divname,idiv7,monthdiv,idiv1,divincome,idiv2,divexpense,idiv3,divbudget,idiv4,divsavings,idiv5,divlimit,idiv6,tempdiv,summarydiv);
         localStorage.setItem("global",JSON.stringify(global));
     }
 
 }
-function addexpense(){
+function addexpense(e){
+    e.preventDefault()
     var item=document.getElementById("expenseitem").value;
     var amount=Number(document.getElementById("expenseamount").value);
     var date=document.getElementById("date").value;
@@ -137,13 +196,15 @@ function addexpense(){
         date1["expense"]=expense;
         date1["budget"]=budget;
         date1["income"]=income;
-        var year1={}
-        year1[year]=date1;
-        currentuser[year]=year1;
+        var month1={}
+        month1[month]=date1;
+        currentuser[year]=month1;
     }
+    
     datewise();
 }
-function addincome(){
+function addincome(e){
+    e.preventDefault()
     var item=document.getElementById("incomeitem").value;
     var amount=Number(document.getElementById("incomeamount").value);
     console.log(item,amount);
@@ -179,13 +240,15 @@ function addincome(){
         date1["expense"]=expense;
         date1["budget"]=budget;
         date1["income"]=income;
-        var year1={}
-        year1[year]=date1;
-        currentuser[year]=year1;
+        var month1={}
+        month1[month]=date1;
+        currentuser[year]=month1;
     }
+    console.log(currentuser);
     datewise();
 }
-function addbudget(){
+function addbudget(e){
+    e.preventDefault()
     var amount=Number(document.getElementById("budgetamount").value);
     var date=document.getElementById("date").value;
     var datearray=date.split("-");
@@ -218,9 +281,9 @@ function addbudget(){
         date1["expense"]=expense;
         date1["budget"]=budget;
         date1["income"]=income;
-        var year1={}
-        year1[year]=date1;
-        currentuser[year]=year1;
+        var month1={}
+        month1[month]=date1;
+        currentuser[year]=month1;
     }
     datewise();
 }
